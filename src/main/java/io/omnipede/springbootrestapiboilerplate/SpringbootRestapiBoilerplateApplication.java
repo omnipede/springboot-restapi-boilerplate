bee98a60284c28be168260665dd4ae0460bc1e73
서현규 <omnipede@coinplug.com>
2020-04-09 10:45:50 +0900

    기능: springboot maven 프로젝트 생성

---
+++ src/main/java/io/omnipede/springbootrestapiboilerplate/SpringbootRestapiBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package io.omnipede.springbootrestapiboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestapiBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestapiBoilerplateApplication.class, args);
+	}
+
+}
