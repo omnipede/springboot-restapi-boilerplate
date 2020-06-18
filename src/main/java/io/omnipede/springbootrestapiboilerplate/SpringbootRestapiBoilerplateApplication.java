e790f4fbecf86a2f6e8b1e60dbbe03a0eac9dc8b
서현규 <omnipede@coinplug.com>
2020-06-18 18:52:14 +0900

    기능: 로그 설정 추가

--- src/main/java/io/omnipede/springbootrestapiboilerplate/SpringbootRestapiBoilerplateApplication.java
+++ src/main/java/io/omnipede/springbootrestapiboilerplate/SpringbootRestapiBoilerplateApplication.java
@@ -8,9 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @see "https://github.com/cheese10yun/spring-guide/blob/master/src/main/java/com/spring/guide/domain/member/application/MemberProfileService.java"
  * TODO: @RequestBody 타입 체크 고안하기 <-
  * TODO: logging 추가, -> AOP 사용하기
- * TODO: request ~ response filter 추가
  * TODO: error response 에 errors 필드 추가하기
- * TODO: 에러 케이스 테스트 클래스 추가
  * @see "https://cheese10yun.github.io/ConstraintValidator/"
  * TODO: DB 추가하기 - @Transactional 어노테이션 사용하기
  * TODO: clean architecture 구현하기
