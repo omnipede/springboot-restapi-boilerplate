11cbaad3dd75cbb07e0138972d4d5741e190ef5e
서현규 <omnipede@coinplug.com>
2020-11-24 17:17:06 +0900

    버그: response charset 이 ISO-8589-1 로 고정되는 버그 해결
    application.properties 에서 encoding charset 을 utf-8로 설정했음에도 ISO-8589-1 로 바뀌는 문제가 있었다.
    이를 해결하기 위해 CharacterEncodingFilter 와 HttpMessageConverter bean 을 등록함

--- src/main/java/io/omnipede/springbootrestapiboilerplate/SpringbootRestapiBoilerplateApplication.java
+++ src/main/java/io/omnipede/springbootrestapiboilerplate/SpringbootRestapiBoilerplateApplication.java
@@ -2,6 +2,14 @@ package io.omnipede.springbootrestapiboilerplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Filter;
 
 /**
  * TODO: @RequestBody json 자료형 확인하는 방법 고안하기
@@ -16,4 +24,22 @@ public class SpringbootRestapiBoilerplateApplication {
 		SpringApplication.run(SpringbootRestapiBoilerplateApplication.class, args);
 	}
 
+	/**
+	 * UTF-8 인코딩을 위해 등록
+	 */
+	@Bean
+	public HttpMessageConverter<String> responseBodyConverter() {
+		return new StringHttpMessageConverter(StandardCharsets.UTF_8);
+	}
+
+	/**
+	 * UTF-8 인코딩을 위해 등록
+	 */
+	@Bean
+	public CharacterEncodingFilter characterEncodingFilter() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return characterEncodingFilter;
+	}
 }
