f51d182d25aff53430198d4bb5178bb2e060713e
서현규 <omnipede@coinplug.com>
2020-11-24 18:07:18 +0900

    기타: 코드 정리

--- src/main/java/io/omnipede/springbootrestapiboilerplate/SpringbootRestapiBoilerplateApplication.java
+++ src/main/java/io/omnipede/springbootrestapiboilerplate/SpringbootRestapiBoilerplateApplication.java
@@ -2,14 +2,7 @@ package io.omnipede.springbootrestapiboilerplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.logging.Filter;
 
 /**
  * TODO: @RequestBody json 자료형 확인하는 방법 고안하기
