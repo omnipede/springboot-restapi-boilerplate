3fa79a22aca7d2f4b9fe8d5da6b511bac06de690
서현규 <omnipede@coinplug.com>
2020-12-07 15:02:33 +0900

    기타: 디렉토리 분리
    Framework 에 독립적인 부분과 의존적인 부분으로 디렉토리를 구분함

---
+++ application/core/src/main/java/io/omnipede/springbootrestapiboilerplate/core/entity/Capacity.java
@@ -0,0 +1,5 @@
+package io.omnipede.springbootrestapiboilerplate.core.entity;
+
+public class Capacity {
+
+}
---
+++ application/core/src/main/java/io/omnipede/springbootrestapiboilerplate/core/entity/Exchange.java
@@ -0,0 +1,4 @@
+package io.omnipede.springbootrestapiboilerplate.core.entity;
+
+public class Exchange {
+}
