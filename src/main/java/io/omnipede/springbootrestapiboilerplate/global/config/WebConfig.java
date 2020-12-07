3a654c51a911b473bcd4b4cf459866189fffbd03
서현규 <omnipede@coinplug.com>
2020-12-07 13:55:25 +0900

    기타: properties -> yaml 로 서버 config 파일 확장자 수정

--- src/main/java/io/omnipede/springbootrestapiboilerplate/global/config/WebConfig.java
+++ src/main/java/io/omnipede/springbootrestapiboilerplate/global/config/WebConfig.java
@@ -6,9 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * No handler found exception 을 처리하기 위해
- * application.properties 파일에
- * spring.mvc.throw-exception-if-no-handler-found=true
- * spring.resources.add-mappings=false
+ * application.yaml 파일에
+ * spring:
+ *  mvc:
+ *   throw-exception-if-no-handler-found: true
+ * resources:
+ *  add-mappings: false
  * ~을 추가함.
  * 이로 인해 static 파일을 찾을 수 없는 문제가 생겼고, 이를 해결하기 위해 다음 configuration 을 추가함
  */
