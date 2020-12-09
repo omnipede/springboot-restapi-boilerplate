43e5123d605dcb26ac4a86c2cd923479dd32cac9
서현규 <omnipede@coinplug.com>
2020-12-09 10:26:49 +0900

    리팩토링: glue 패키지를 entrypoint.bean 으로 네이밍 수정

---
+++ application/infra/src/main/java/io/omnipede/springbootrestapiboilerplate/entrypoint/bean/DatabaseDataProviderConfiguration.java
@@ -0,0 +1,22 @@
+package io.omnipede.springbootrestapiboilerplate.entrypoint.bean;
+
+import io.omnipede.dataprovider.database.broadbandaccessdevice.BroadbandAccessDeviceDatabaseDataProvider;
+import io.omnipede.dataprovider.database.exchange.ExchangeDatabaseDataProvider;
+import io.omnipede.dataprovider.jpa.broadbandaccessdevice.BroadbandAccessDeviceRepository;
+import io.omnipede.dataprovider.jpa.broadbandaccessdevice.ExchangeRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseDataProviderConfiguration {
+
+    @Bean
+    public BroadbandAccessDeviceDatabaseDataProvider broadbandAccessDeviceDatabaseDataProvider(BroadbandAccessDeviceRepository broadbandAccessDeviceRepository, ExchangeRepository exchangeRepository) {
+        return new BroadbandAccessDeviceDatabaseDataProvider(broadbandAccessDeviceRepository, exchangeRepository);
+    }
+
+    @Bean
+    public ExchangeDatabaseDataProvider exchangeDatabaseDataProvider(ExchangeRepository exchangeRepository) {
+        return new ExchangeDatabaseDataProvider(exchangeRepository);
+    }
+}
---
+++ application/infra/src/main/java/io/omnipede/springbootrestapiboilerplate/entrypoint/bean/UseCaseConfig.java
@@ -0,0 +1,23 @@
+package io.omnipede.springbootrestapiboilerplate.entrypoint.bean;
+
+import io.omnipede.core.usecase.broadbandaccessdevice.getdetails.GetBroadbandAccessDeviceDetailsUseCase;
+import io.omnipede.core.usecase.broadbandaccessdevice.getdetails.GetDeviceDetails;
+import io.omnipede.core.usecase.exchange.getcapacity.DoesExchangeExist;
+import io.omnipede.core.usecase.exchange.getcapacity.GetAvailablePortsOfAllDevicesInExchange;
+import io.omnipede.core.usecase.exchange.getcapacity.GetCapacityForExchangeUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UseCaseConfig {
+
+    @Bean
+    public GetBroadbandAccessDeviceDetailsUseCase getBroadbandAccessDeviceDetailsUseCase(GetDeviceDetails getDeviceDetails) {
+        return new GetBroadbandAccessDeviceDetailsUseCase(getDeviceDetails);
+    }
+
+    @Bean
+    public GetCapacityForExchangeUseCase getCapacityForExchangeUseCase(DoesExchangeExist doesExchangeExist, GetAvailablePortsOfAllDevicesInExchange getAvailablePortsOfAllDevicesInExchange) {
+        return new GetCapacityForExchangeUseCase(doesExchangeExist, getAvailablePortsOfAllDevicesInExchange);
+    }
+}
